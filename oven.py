049e49454de3ef793c8df779bb99138cb2bbd019
Stefano Martina <stefano.martina@gmail.com>
2016-01-25 01:44:01 +0100

    Implemented simulated annealing process, and statistical state move. Still need to implement the calculus of the energy of a path.

--- oven.py
+++ oven.py
@@ -1,8 +1,27 @@
 import numpy as np
 
 class Oven:
-    def __init__(self, initialPath=np.array([])):
-        self._initialPath = initialPath
+    def __init__(self, path):
+        self._path = path
 
-    def annealing(self, initialTemperature=1000, warmingRatio=0.8, trials=100):
-        pass
+    def annealing(self, initialTemperature=1000, warmingRatio=0.8, trials=100, minTemperature=1, minDeltaEnergy=10):
+        """
+        Run the simulated annealing process. Start from given initial
+        temperature and start a loop. In each iteration make a certain
+        amount of statistic moves (trial), then decrease the
+        temperature by a certain factor and then continue to next
+        iteration. The loop ends when a certain minimal temperature is
+        reached, or when the difference between the energy of the
+        initial configuration and the final configuration, is less
+        than a certain treshold.
+        """
+        temperature = initialTemperature
+        while true:
+            initialEnergy = self._path.energy()
+            for i in range(trials):
+                self._path.tryMove(temperature)
+
+            finalEnergy = self._path.energy()
+            temperature = temperature * warmingRatio
+            if (temperature < minTemperature) or (abs(initialEnergy - finalEnergy) < minDeltaEnergy):
+                break
--- path.py
+++ path.py
@@ -1,10 +1,49 @@
 import smoothener
+import random
+import math
+import numpy as np
 
 class Path:
+    _angleSlices = 10
+    _distanceStep = 1
     def __init__(self, initialVertexes):
         self._smoothener = smoothener.Smoothener()
         self._vertexes = initialVertexes
+        self._currentEnergy = self._calculatePathEnergy(initialVertexes)
 
+    def energy(self):
+        return self._currentEnergy
+    
+    def tryMove(self, temperature):
+        """
+        Move the path in a neighbouring state, with a certain
+        acceptance probability.
+        Pick a random vertex (except extremes), and move
+        it in a random direction (discretized with a certain treshold)
+        by a distance step.
+        """
+        newPath = np.copy(self._vertexes)
+        moveIndex = random.randint(1,len(newPath-2))
+        moveAngle = random.randint(0,self._angleSlices-1)*math.pi/self._angleSlices
+
+        newPath[moveIndex][0] = newPath[moveIndex][0] + math.cos(moveAngle) * self._distanceStep
+        newPath[moveIndex][1] = newPath[moveIndex][1] + math.sin(moveAngle) * self._distanceStep
+
+        newEnergy = self._calculatePathEnergy(newPath)
+        if (newEnergy < self._currentEnergy) or (math.exp(-(newEnergy-self._currentEnergy)/temperature) >= random.random()):
+            self._vertexes = newPath
+            self._currentEnergy = newEnergy
+        
+    def _calculatePathEnergy(self, vertexes):
+        """
+        calculate the energy of the passed path and returns it. Use a
+        lagrangian relaxation because we neet to evaluate
+        min(len(path)) given the costraint that all quadrilaters
+        formed by 4 consecutive points in the path must be collision
+        free.
+        """
+        return 0
+        
     def plot(self, plotter, plotStartEnd=True, plotInnerVertexes=False, plotEdges=True, plotSpline=True):
         if self._vertexes.size > 0:
             self._smoothener.plot(self._vertexes, plotter, plotStartEnd, plotInnerVertexes, plotEdges, plotSpline)
