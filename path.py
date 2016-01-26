dd0bd44b8ca6e9e1780b2f9aa18f4094b19831c0
Stefano Martina <stefano.martina@gmail.com>
2016-01-26 20:54:28 +0100

    Adjusted statistical state move, prepared for lagrangian relaxation.

--- path.py
+++ path.py
@@ -4,44 +4,70 @@ import math
 import numpy as np
 
 class Path:
-    _angleSlices = 10
-    _distanceStep = 1
+    """
+    Represents a state of system in the lagrangian space (path
+    configurations X costraint).
+    """
+    _maxVlambdaPert = 1.
+    _maxVertexPert = 1.
+    _initialVlambda = 0.
+    _changeVlambdaProbability = 0.05
+    
     def __init__(self, initialVertexes):
         self._smoothener = smoothener.Smoothener()
         self._vertexes = initialVertexes
-        self._currentEnergy = self._calculatePathEnergy(initialVertexes)
+        self._dimR = self._vertexes.shape[0]
+        self._dimC = self._vertexes.shape[1]
+        self._vlambda = np.ones(self._vertexes.shape) * self._initialVlambda 
+        self._currentEnergy = self._calculatePathEnergy(self._vertexes, self._vlambda)
 
     def energy(self):
         return self._currentEnergy
     
     def tryMove(self, temperature):
         """
-        Move the path in a neighbouring state, with a certain
-        acceptance probability.
+        Move the path or lambda multipiers in a neighbouring state,
+        with a certain acceptance probability.
         Pick a random vertex (except extremes), and move
-        it in a random direction (discretized with a certain treshold)
-        by a distance step.
+        it in a random direction (with a maximum perturbance).
         """
-        newPath = np.copy(self._vertexes)
-        moveIndex = random.randint(1,len(newPath-2))
-        moveAngle = random.randint(0,self._angleSlices-1)*math.pi/self._angleSlices
 
-        newPath[moveIndex][0] = newPath[moveIndex][0] + math.cos(moveAngle) * self._distanceStep
-        newPath[moveIndex][1] = newPath[moveIndex][1] + math.sin(moveAngle) * self._distanceStep
+        moveR = random.randint(1,self._dimR - 2) #don't change extremes
+        moveC = random.randint(0,self._dimC - 1)
 
-        newEnergy = self._calculatePathEnergy(newPath)
-        if (newEnergy < self._currentEnergy) or (math.exp(-(newEnergy-self._currentEnergy)/temperature) >= random.random()):
-            self._vertexes = newPath
-            self._currentEnergy = newEnergy
+        if random.random() < self._changeVlambdaProbability:
+            newVlambda = np.copy(self.vlambda)
+            newVlambda[moveR][moveC] = newVlambda[moveR][moveC] + (random.uniform(-1.,1.) * self._maxVlambdaPert)
+
+            newEnergy = self._calculatePathEnergy(self._vertexes, newVlambda)
+
+            #attention, different formula
+            if (newEnergy > self._currentEnergy) or (math.exp(-(self._currentEnergy-newEnergy)/temperature) >= random.random()):
+                self._vlambda = newVlambda
+                self._currentEnergy = newEnergy
         
-    def _calculatePathEnergy(self, vertexes):
+        else:
+            newVertexes = np.copy(self._vertexes)
+            newVertexes[moveR][moveC] = newVertexes[moveR][moveC] + (random.uniform(-1.,1.) * self._maxVertexPert)
+
+            newEnergy = self._calculatePathEnergy(newVertexes, self._vlambda)
+
+            #attention, different formula
+            if (newEnergy < self._currentEnergy) or (math.exp(-(newEnergy-self._currentEnergy)/temperature) >= random.random()):
+                self._vertexes = newVertexes
+                self._currentEnergy = newEnergy
+        
+    def _calculatePathEnergy(self, vertexes, vlambda):
         """
         calculate the energy of the passed path and returns it. Use a
-        lagrangian relaxation because we neet to evaluate
-        min(len(path)) given the costraint that all quadrilaters
+        lagrangian relaxation because we need to evaluate
+        min(meanAngle(path)) given the costraint that all quadrilaters
         formed by 4 consecutive points in the path must be collision
-        free.
+        free; where meanAngle(path) is the mean of the supplementary
+        angles of each pair of edges of the path.
         """
+
+        
         return 0
         
     def plot(self, plotter, plotStartEnd=True, plotInnerVertexes=False, plotEdges=True, plotSpline=True):
